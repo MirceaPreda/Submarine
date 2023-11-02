0f08b2bd6c4769793d9ed1e670d1b4c39a35c5a2
MirceaPreda <nito374@gmail.com>
2023-11-02 23:39:45 +0200

    Updates and improvements
    * refactored the transformer
    * added data cleaning

--- engine/fuelTank/transformer.py
+++ engine/fuelTank/transformer.py
@@ -2,45 +2,45 @@ import json
 import csv
 import glob
 from datetime import datetime
-import pandas as pd
 
-#data = ex.dataExtractor()
+#global variables
+numberOfFiles = []
+dataForTransformation = []
+transformationDate = datetime.today().strftime('%Y%m%d%H%M%S')
 
-
-# transform existing files to match the needed data
-# create new file with the values for dataframes later
-
+#prepare the data
 def baseData():
-    #arrays and variables
-    transformationDate = datetime.today().strftime('%Y%m%d%H%M%S')
-
-    dataForTransformation = []
-    numberOfFiles = []
-
-
     try:
         extractedFiles = glob.glob('./engine/fuel/*.json')
         for file in extractedFiles:
             openFile = open(file)
             loadJSON = json.load(openFile)
             numberOfFiles.append(loadJSON)
-            for elementIndex, element in enumerate(loadJSON):
-                  with open(f'./engine/propeller/baseDataFrom{transformationDate}.csv', 'a', newline='') as individualFile:
-                        dataWriter = csv.writer(individualFile)
-                        # headers
-                        currentWeatherHeader = element['current_weather']
-                        # base data
-                        currentLatitude = element['latitude']
-                        currentLongitude = element['longitude']
-                        currentTemperature = currentWeatherHeader['temperature']
-                        currentWindspeed = currentWeatherHeader['windspeed']
-                        isItDay = currentWeatherHeader['is_day']
-
-                        dataForTransformation.append([currentLatitude, currentLongitude, currentTemperature, currentWindspeed, isItDay])
-                        dataWriter.writerow(dataForTransformation[elementIndex])
-                        # BUG comes from index of elementIndex -> does not pass all the items
-                        print(dataForTransformation[elementIndex])
+            #print(numberOfFiles)
+        return numberOfFiles
     except:
-        print("Issue opening the file for transformation or with the transformation")
+        print('Issue loading extracted file')
+
+#transform the data to a CSV
+def transformer():
+    try:
+        for element in numberOfFiles:
+                  with open(f'./engine/propeller/baseDataFrom{transformationDate}.csv', 'a+', newline='') as individualFile:
+                        dataWriter = csv.writer(individualFile)
+                        for index, item in enumerate(element):
+                        # headers
+                            currentWeatherHeader = item['current_weather']
+                        # base data
+                            currentLatitude = item['latitude']
+                            currentLongitude = item['longitude']
+                            currentTemperature = currentWeatherHeader['temperature']
+                            currentWindspeed = currentWeatherHeader['windspeed']
+                            isItDay = currentWeatherHeader['is_day']
+
+                            dataForTransformation = [currentLatitude, "{:.2f}".format(currentLongitude), currentTemperature, currentWindspeed, isItDay]
+                            dataWriter.writerow(dataForTransformation)
+    except:
+        print('Issue transforming data')
 
 baseData()
+transformer()
--- radar/dataCleaner.py
+++ radar/dataCleaner.py
@@ -6,19 +6,16 @@ import glob
 
 def dataCleaner():
 
-    headers = ['Latitude', 'Longitude', 'Temperature', 'Windspeed', 'Day']
-    allFiles = glob.glob('./engine/fuel/*.json')
+    tableHeaders = ['Latitude', 'Longitude', 'Temperature', 'Windspeed', 'Day']
+    allFiles = glob.glob('./engine/propeller/*.csv')
     dfs = []
     #try:
     for file in allFiles:
-            readFile = pd.read_json(file, lines=True)
+            readFile = pd.read_csv(file, names=tableHeaders)
             print(readFile.to_string())
 
             temp = dfs.append(readFile)
             print(temp)
-
-            completeData = pd.json_normalize(temp)
-            print(completeData)
     #except:
     print('Issue cleaning the data')
 
